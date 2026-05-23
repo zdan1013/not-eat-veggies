4e6f0db531c8aae3b11cc6f708969245e45ad125
zhudan <zhudan@192.168.124.6>
2026-05-23 09:05:30 +0800

    Align open-mouth walk frames

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ const playerSpriteHeight = 225;
 const playerGroundOffset = 116;
 const playerMouthOffset = { x: 110, y: 9 };
 const itemMouthHeight = playerGroundOffset - playerMouthOffset.y;
+const playerOpenFrameYOffset = [19, 46, 19, 31];
 const maxEnergy = 100;
 const foodEnergy = 5;
 const assetStatus = {
@@ -473,10 +474,11 @@ function drawKittenSprite(jumpPose) {
   const scale = playerSpriteHeight / image.naturalHeight;
   const feetX = 56;
   const feetY = 123;
+  const yOffset = openFrame ? playerOpenFrameYOffset[game.eating.frameIndex] * scale : 0;
   const drawW = image.naturalWidth * scale;
   const drawH = image.naturalHeight * scale;
 
-  ctx.drawImage(image, feetX - drawW * 0.5, feetY - drawH, drawW, drawH);
+  ctx.drawImage(image, feetX - drawW * 0.5, feetY - drawH + yOffset, drawW, drawH);
 }
 
 function jumpFrameIndex(airTime, jumpPose) {
